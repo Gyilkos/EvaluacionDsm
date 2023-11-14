e66237caa768de05c7d40c815edaa4c93dc8d140
Nicolas Robles <nicolas.robles@alumnos.ucn.cl>
2023-11-14 08:51:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
@@ -0,0 +1,98 @@
+package com.example.evaluaciondsm.ui.home;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.evaluaciondsm.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+
+public class HomeFragment extends Fragment {
+
+    private EditText etCode, etName, etPrice;
+    private Button btnRegister, btnSearchP, btnDelete, btnEdit;
+    private ArrayList <String> listProducts;
+    private ArrayAdapter <String> adapter;
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        binding.btnSearchP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search();
+            }
+        });
+
+        binding.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                delete();
+            }
+        });
+
+        binding.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edit();
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    //Metodo para guardar los productos
+    public void save() {
+
+
+    }
+
+    //Metodo para buscar productos
+    public void search() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void edit() {
+
+    }
+
+    public void cleanForm() {
+    }
+
+}
